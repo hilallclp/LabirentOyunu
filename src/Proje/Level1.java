7fc8b10025db0e3240b5ab41df4ec00d7005e0d1
hilallclp <hllclp49@gmail.com>
2026-03-16 00:11:48 +0300

    Update Level1.java

--- src/Proje/Level1.java
+++ src/Proje/Level1.java
@@ -7,15 +7,6 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-
-
-import java.awt.Graphics;
-import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 class Level1 extends Level {
     @Override
     public void initLevel() {
